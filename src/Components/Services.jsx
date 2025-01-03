2b4cf399227abaf920ead347e0953e6d18b8ab12
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2025-01-03 09:07:26 +0500

    changes-services4

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -19,7 +19,7 @@ export default function Services() {
           <h4>CSS Templates</h4>
           <p>
             TemplateMo website is the best for you to explore and download free
-            website templates website jksjka kjsak sahjahsjahsjahs sajh.
+            website templates website jksjka kjsak sahjahsjahsjahs sajh djshjshdjs sdjhsjd .
           </p>
         </div>
       </div>
