ba3515e013b81627c7953eee09ec931f554dfc51
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2025-01-17 13:39:19 +0500

    changes-75

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -59,6 +59,7 @@ export default function Banner() {
                       <div className="buttons">
                         <div className="green-button">
                           <a href="#">Discover More</a>
+                          <a href="#">Discover More si the important</a>
                         </div>
                         <div className="orange-button">
                           <a href="#">Contact Us</a>
