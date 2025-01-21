7dce3f0cb1bab36c8829c361decc8424232d7756
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-01-21 23:22:55 +0500

    changes-banner-35

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -61,7 +61,7 @@ export default function Banner() {
                           <a href="#">Discover More</a>
                         </div>
                         <div className="orange-button">
-                          <a href="#">Contact Us and why contact dear</a>
+                          <a href="#">Contact Us</a>
                         </div>
                       </div>
                     </div>
